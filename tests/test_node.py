12d8878fc5efd482d8af67255928ddc0fd2e54be
Sebastian Allard <sebastian@seallard.dev>
2020-09-22 20:02:14 +0200

    Add tests for node mutations

--- tests/test_node.py
+++ tests/test_node.py
@@ -7,9 +7,15 @@ def test_node_initialisation(genome):
     inputs = genome.num_inputs
     outputs = genome.num_outputs
     assert len(genome.nodes) == inputs + outputs, "correct number of nodes"
-    assert [x.innovation_number for x in genome.nodes] == [x for x in range(inputs + outputs)], "correct node innovation ids"
-    assert [x.node_type for x in genome.nodes[:inputs]] == [NodeType.INPUT for x in range(inputs)], "correct type for inputs"
-    assert [x.node_type for x in genome.nodes[inputs:]] == [NodeType.OUTPUT for x in range(outputs)], "correct type for outputs"
+
+    node_numbers = [x.innovation_number for x in genome.nodes]
+    assert node_numbers == [x for x in range(inputs + outputs)], "correct node innovation ids"
+
+    node_input_types = [x.node_type for x in genome.nodes[:inputs]]
+    assert node_input_types == inputs*[NodeType.INPUT], "correct type for inputs"
+
+    node_output_types = [x.node_type for x in genome.nodes[inputs:]]
+    assert node_output_types == outputs*[NodeType.OUTPUT], "correct type for outputs"
 
 
 def test_add_single_node():
@@ -23,7 +29,8 @@ def test_add_single_node():
 
     assert len(genome.nodes) == original_number_of_nodes + 1, "one node added"
     assert len(genome.links) == original_number_of_links + 2, "two links added"
-    assert False in [x.enabled for x in genome.links], "one link disabled"
+    disabled_links = [link for link in genome.links if not link.enabled]
+    assert len(disabled_links) == 1, "one link disabled"
     assert new_in.to_node == new_node, "new link into new node"
     assert new_out.from_node == new_node, "new link out of new node"
     assert new_in.weight == 1, "weight of new in link is 1"
@@ -31,4 +38,28 @@ def test_add_single_node():
 
 
 def test_add_multiple_nodes():
-    pass
+    genome = Genome(id=1, num_inputs=1, num_outputs=1)
+    genome.mutate_add_node(tries=1)
+    genome.mutate_add_node(tries=50) # Avoid disabled link
+
+    assert len(genome.nodes) == 4, "two nodes added"
+    assert len(genome.links) == 5, "four new links added"
+
+    disabled_links = [link for link in genome.links if not link.enabled]
+    assert len(disabled_links) == 2, "two links disabled"
+    assert not genome.links[0].enabled, "original link disabled"
+
+    recurrent_nodes = [node for node in genome.nodes if node.recurrent]
+    assert len(recurrent_nodes) == 0, "no node is marked as recurrent"
+
+    recurrent_links = [link for link in genome.links if link.recurrent]
+    assert len(recurrent_links) == 0, "no link is marked as recurrent"
+
+
+def test_impossible_add_node():
+    genome = Genome(id=1, num_inputs=1, num_outputs=1)
+    genome.links[0].enabled = False # Disable the only existing link
+    genome.mutate_add_node(tries=50)
+
+    assert len(genome.nodes) == 2, "no new node added"
+    assert len(genome.links) == 1, "no new link added"
