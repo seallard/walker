43c49bbe18048fbcaee2f6b86106f52798af3d2b
seallard <sebastian@seallard.dev>
2020-10-07 15:25:22 +0200

    Add activation count in nodes

--- neat/node.py
+++ neat/node.py
@@ -10,6 +10,7 @@ class Node:
         self.type = node_gene.type
         self.id = node_gene.id
         self.output = 0
+        self.activation_count = 0
 
 
     def activate(self):
@@ -19,6 +20,7 @@ class Node:
             signal += link.from_node.output * link.weight
 
         self.output = 1 / (1 + exp(-signal))
+        self.activation_count += 1
 
     def is_output(self):
         return self.type == NodeType.OUTPUT
