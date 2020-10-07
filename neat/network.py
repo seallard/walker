a47441849200b9115f1569a0114a9b74406963ee
seallard <sebastian@seallard.dev>
2020-10-07 10:54:30 +0200

    Add visualisation of network

--- neat/network.py
+++ neat/network.py
@@ -1,3 +1,5 @@
+import networkx as nx
+from neat.enums.node_types import NodeType
 
 
 class Network:
@@ -24,4 +26,23 @@ class Network:
         return outputs
 
     def draw(self):
-        pass
+        graph = nx.DiGraph()
+        graph.add_nodes_from(self.nodes)
+
+        color_map = []
+        for node in self.nodes:
+            if node.type == Nodetype.INPUT:
+                color_map.append('green')
+
+            elif node.type == NodeType.HIDDEN:
+                color_map.append('blue')
+            else:
+                color_map.append("red")
+
+        for link in self.links:
+            graph.add_edge(link.from_node, link.to_node)
+
+
+        positioning = nx.spring_layout(graph)
+        nx.draw(graph, pos=positioning, edge_color='black', arrowsize=5, arrowstyle='fancy', node_color=color_map)
+        plt.show()
