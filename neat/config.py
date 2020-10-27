f63ea8c78d4a21c84ce73ef5caebb0abcb2c8fe5
seallard <sebastian@seallard.dev>
2020-10-27 14:13:50 +0100

    Group settings

--- neat/config.py
+++ neat/config.py
@@ -8,40 +8,71 @@ class Config():
         with open(file, 'r') as f:
             config = json.load(f)
 
-        self.population_size = config['population_size']
+        # Number of genomes in the population.
+        self.population_size = config['population_settings']['size']
 
-        self.weight_mutation_range = config['weight_mutation_range']
-        self.weight_mutation_probability = config['weight_mutation_probability']
-        self.weight_replacement_rate = config['weight_replacement_rate']
 
-        self.add_node_probability = config['add_node_probability']
-        self.add_link_probability = config['add_link_probability']
+        #### SPECIES SETTINGS ####
+        species_settings = config['species_settings']
 
-        self.node_add_tries = config['node_addition_attempts']
-        self.link_add_tries = config['link_addition_attempts']
+        self.species_target_number = species_settings['target_number'] # TODO: Not used.
 
-        self.link_recurrent_probability = config['link_recurrent_probability']
-        self.reenable_link_probability = config['link_reenable_probability']
-        self.toggle_probability = config['link_toggle_enable_probability']
-        self.mutate_only_probability = config['mutate_only_probability']
-        self.mate_only = config['mate_only_probability']
-        self.interspecies_mating_rate = config['interspecies_mating_rate']
-        self.mate_by_choosing = config['mate_by_choosing_probability']
-        self.mate_by_averaging = config['mate_by_averaging_probability']
+        # Maximum number of generations a species is allowed to stay at the same max fitness before it is removed.
+        self.maximum_stagnation = species_settings['maximum_stagnation']
 
-        self.compatibility_threshold = config['compatibility_threshold']
-        self.c_excess = config['excess_coefficient']
-        self.c_disjoint = config['disjoint_coefficient']
-        self.c_weight = config['weight_coefficient']
-        self.survival_threshold = config['survival_threshold']
-        self.maximum_stagnation = config['maximum_stagnation']
-        self.species_target_number = config['target_number_of_species']
+        # Coefficients representing how important the gene differences are in measuring compatibility.
+        self.c_excess = species_settings['excess_coefficient']
+        self.c_disjoint = species_settings['disjoint_coefficient']
+        self.c_weight = species_settings['weight_coefficient']
+        self.compatibility_threshold = species_settings['compatibility_threshold']
 
-        self.environment = config['environment']
-        self.num_inputs = config['input_dimension']
-        self.num_outputs = config['output_dimension']
 
-        self.young_threshold = config['young_threshold']
-        self.young_boost = config['young_boost']
-        self.old_threshold = config['old_threshold']
-        self.old_penalty = config['old_penalty']
+        #### REPRODUCTION SETTINGS ####
+        reproduction_settings = config['reproduction_settings']
+
+        # Percentage of each species allowed to reproduce.
+        self.survival_threshold = reproduction_settings['survival_threshold']
+
+        # Probability an offspring will be created only through crossover without mutation.
+        self.mate_only_probability = reproduction_settings['mate_only_probability']
+
+        # Probability an offspring will be created only through mutation without crossover.
+        self.mutate_only_probability = reproduction_settings['mutate_only_probability']
+
+        # Percentage of crossovers allowed to occur between parents of different species.
+        self.interspecies_mating_rate = reproduction_settings['interspecies_mating_rate'] # TODO: not used.
+
+        # Probability that genes will be chosen one at a time from either parent during crossover.
+        self.mate_by_choosing = reproduction_settings['mate_by_choosing_probability'] # TODO: not used.
+
+        # Probability that matching genes will be averaged during crossover.
+        self.mate_by_averaging = reproduction_settings['mate_by_averaging_probability'] # TODO: not used.
+
+
+
+        #### MUTATION SETTINGS ####
+        mutation_settings = config['mutation_settings']
+
+        # Probability a new connection will be added to the genome.
+        self.add_link_probability = mutation_settings['add_link_probability']
+        self.link_add_tries = mutation_settings['link_addition_attempts']
+
+        # Probability a new node gene will be added to the genome
+        self.add_node_probability = mutation_settings['add_node_probability']
+        self.node_add_tries = mutation_settings['node_addition_attempts']
+
+        # The maximum magnitude of a mutation that changes the weight of a connection.
+        self.weight_mutation_range = mutation_settings['weight_mutation_range']
+        self.weight_mutation_probability = mutation_settings['weight_mutation_probability']
+        self.weight_replacement_rate = mutation_settings['weight_replacement_rate']
+
+        # Probability a new connection will be recurrent.
+        self.link_recurrent_probability = mutation_settings['link_recurrent_probability']
+        self.reenable_link_probability = mutation_settings['link_reenable_probability']
+        self.toggle_probability = mutation_settings['link_toggle_enable_probability']
+
+        #### ENVIRONMENT SETTINGS ####
+        environment_settings = config['environment_settings']
+        self.environment = environment_settings['environment']
+        self.num_inputs = environment_settings['input_dimension']
+        self.num_outputs = environment_settings['output_dimension']
